1362716578aba1bfcb7dd857587932d646c1f49c
Adam <103382498+HurasAdam@users.noreply.github.com>
2022-11-05 02:59:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,50 +6,38 @@ let counter =0;
 const arr =[]
 
 
-//making new div with paragraph inside, adding classes and pushing both into div-wrapper
+//making new div and li wih classes and pushing both into div-wrapper
 addButton.addEventListener('click',function(){
-    const newDiv= document.createElement('div');
-    newDiv.classList.add('newDiv');
-    wrapper.appendChild(newDiv);
-    const toDoList= document.createElement('li');
-    toDoList.classList.add('toDoList')
-    newDiv.appendChild(toDoList);
-    arr.push(newDiv);
-    toDoList.innerHTML= inputField.value;
-    
-    
+const newDiv= document.createElement('div');
+newDiv.classList.add('newDiv');
+wrapper.appendChild(newDiv);
+const toDoList= document.createElement('li');
+toDoList.classList.add('toDoList')
+newDiv.appendChild(toDoList);
+arr.push(newDiv);
+toDoList.innerHTML= inputField.value;
+const checkedButton= document.createElement('button')
+const deleteButton= document.createElement('button')
+deleteButton.classList.add('deleteButton');
+checkedButton.classList.add('checkedButton')
+newDiv.appendChild(checkedButton);
+newDiv.appendChild(deleteButton);
+deleteButton.setAttribute('id',counter);
+counter++
+deleteButton.innerHTML='<img src="images/bin.png" alt="">'
+checkedButton.innerHTML= '<img src="images/done.png" alt="">'
+render()
 
-    
-    const checkedButton= document.createElement('button')
-    const deleteButton= document.createElement('button')
-    deleteButton.classList.add('deleteButton');
-    checkedButton.classList.add('checkedButton')
-    newDiv.appendChild(checkedButton);
-    newDiv.appendChild(deleteButton);
-    
-   
-    deleteButton.setAttribute('id',counter);
-    
-    counter++
-   
-    
-    deleteButton.innerHTML='<img src="images/bin.png" alt="">'
-    
-    checkedButton.innerHTML= '<img src="images/done.png" alt="">'
-    render()
-    checkedButton.addEventListener('click',function(e){
+checkedButton.addEventListener('click',function(e){
         toDoList.classList.toggle('active');
     })
 
 //delete-button listener
 deleteButton.addEventListener('click',function(e){
-    console.log(e.target) 
-    const index=e.target.id;
-     arr[index]=null;
-
-
-     
-     render()
+ 
+const index=e.target.id;
+arr[index]=null;
+render()
      
 })
 })
